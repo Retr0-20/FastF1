f109dbedbf68d93925089a4194be899ff82a3860
Retr0-20 <github.expedited230@passmail.net>
2026-05-29 13:02:42 +0100

    Added FastF1 caching and API error handling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# http://127.0.0.1:8000/laps/2024/Monaco/R
-
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import fastf1
 
 app = FastAPI()
@@ -10,14 +8,20 @@ CACHE_DIR = Path("fastf1_cache")
 CACHE_DIR.mkdir(exist_ok=True)
 fastf1.Cache.enable_cache(str(CACHE_DIR))
 
+#http://127.0.0.1:8000/
 @app.get("/")
 def home():
     return {"message": "FastAPI + FastF1 working"}
 
+# http://127.0.0.1:8000/laps/2024/Monaco/R
+# http://127.0.0.1:8000/laps/2024/Monaco/R?limit=100
 @app.get("/laps/{year}/{event}/{session_type}")
 def get_laps(year: int, event: str, session_type: str):
-    session = fastf1.get_session(year, event, session_type)
-    session.load()
+    try:
+        session = fastf1.get_session(year, event, session_type)
+        session.load()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     laps = session.laps[[
         "Driver",
@@ -30,10 +34,14 @@ def get_laps(year: int, event: str, session_type: str):
         "TyreLife"
     ]]
 
-    return laps.head().astype(str).to_dict(orient="records")
+    return laps.astype(str).to_dict(orient="records")
 
+# http://127.0.0.1:8000/schedule/2024
 @app.get("/schedule/{year}")
 def get_schedule(year: int):
-    schedule = fastf1.get_event_schedule(year)
+    try:
+        schedule = fastf1.get_event_schedule(year)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     return schedule.astype(str).to_dict(orient="records")
