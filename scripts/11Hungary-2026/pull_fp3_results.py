08213dda3a3ea9005c73e99160e33e07ba3b1406
Retr0-20 <github.expedited230@passmail.net>
2026-07-23 15:14:11 +0100

    Changed all references to Hungary in new folder

--- scripts/11Hungary-2026/pull_fp3_results.py
+++ scripts/11Hungary-2026/pull_fp3_results.py
@@ -9,7 +9,7 @@ CACHE_DIR.mkdir(exist_ok=True)
 fastf1.Cache.enable_cache(str(CACHE_DIR))
 
 YEAR = 2026
-EVENT = "Belgium"
+EVENT = "Hungary"
 SESSION_TYPE = "FP3"
 
 OUTPUT_PATH = PROJECT_ROOT / f"data/processed/{YEAR}_{EVENT}_{SESSION_TYPE}_fastest_laps_by_driver.csv"
