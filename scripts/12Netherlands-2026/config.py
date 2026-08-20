643344d8eed469877120ef941ab770883b73f091
Retr0-20 <github.expedited230@passmail.net>
2026-08-20 21:31:46 +0100

    Created a file for configuration, so instead of going into each file and changing what year and race, I can change it once.
    This has been tested with pull_fp1_results and will be adjusted for all files.

---
+++ scripts/12Netherlands-2026/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# Configurable information for Grand Prixs
+
+YEAR = 2025
+EVENT = "Netherlands"
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+CACHE_DIR = PROJECT_ROOT / "fastf1_cache"
