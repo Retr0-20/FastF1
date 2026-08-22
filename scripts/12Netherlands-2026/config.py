fb60131cf3f15de9bb3e4c632c0627791904e71d
Retr0-20 <github.expedited230@passmail.net>
2026-08-22 20:33:00 +0100

    Tested this new interpretation of the file with Qualifying. It also works, it may need some separation for different formats

--- scripts/12Netherlands-2026/config.py
+++ scripts/12Netherlands-2026/config.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 # Configurable information for Grand Prixs
 
-YEAR = 2025
+YEAR = 2026
 EVENT = "Netherlands"
-SESSION_TYPE = "FP2"
+SESSION_TYPE = "FP1"  # Options: FP1, FP2, FP3, Q, R
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 CACHE_DIR = PROJECT_ROOT / "fastf1_cache"
