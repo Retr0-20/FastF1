a79a06f1396b56ef47de3fb7cc6c2bc1144f48c6
Retr0-20 <github.expedited230@passmail.net>
2026-08-26 16:52:16 +0100

    Added naming convention automation for output folder to add new files to a folder for corresponding race events

--- scripts/12Netherlands-2026/config.py
+++ scripts/12Netherlands-2026/config.py
@@ -7,3 +7,6 @@ EVENT = "Netherlands"
 SESSION_TYPE = "R"  # Options: FP1, FP2, FP3, Q, R, SQ, S
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 CACHE_DIR = PROJECT_ROOT / "fastf1_cache"
+
+def get_event_folder():
+    return f"{YEAR}_{EVENT.lower()}"
