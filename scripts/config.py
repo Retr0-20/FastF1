ec87672b0b9e5425113d61d11683fca78c599f38
Retr0-20 <github.expedited230@passmail.net>
2026-09-05 14:16:44 +0100

    Pulled FP1 results for Monza

--- scripts/config.py
+++ scripts/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # ---------------------------------------------------------------------
 
 YEAR = 2026
-EVENT = "Netherlands"
+EVENT = "Monza"
 SESSION_TYPE = "FP1"  # Options: FP1, FP2, FP3, Q, R, SQ, S
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
